9f78663504c07de39051fbc02c7603c50d0a4957
AkhileshPandeyji <pandeyakhilesh5327@gmail.com>
2020-02-27 19:06:02 +0530

    treefy added

--- lec2/Activity/commands/untreefy.js
+++ lec2/Activity/commands/untreefy.js
@@ -5,26 +5,32 @@ var uniqid = require("uniqid");
 module.exports.untreefy = function(){
     let src = arguments[0];
     let dest = arguments[1];
-
-    untreefyDir(src,dest);
+    let root={};
+    untreefyDir(src,dest,root);
+    fs.writeFileSync(path.join(dest,"metadata.json"),JSON.stringify(root));
 }
 
-function untreefyDir(src,dest){
-    var arr=[];
-    var ans = fs.lstatSync(src).isFile();
-    if(ans){
-        let filename = uniqid();
-        let newPath = path.join(dest,filename);
-        fs.copyFileSync(src,newPath);
+function untreefyDir(src,dest,node){
+    let ans = fs.lstatSync(src).isDirectory();
+    if(ans==false){
+        let uniqname = uniqid();
+        node.isFile=true;
+        node.name=path.basename(src);
+        node.newName=uniqname;
+        fs.copyFileSync(src,path.join(dest,uniqname));
     }
     else{
-        let children = fs.readdirSync(src);
-        for(let i=0;i<children.length;i++){
-            let pathname = path.join(src,children[i]);
-            untreefyDir(pathname,dest);
+        node.isFile=false;
+        node.name = path.basename(src);
+        node.children=[];
+        let childrens = fs.readdirSync(src);
+        for(let i=0;i<childrens.length;i++){
+            let chobj = {};
+            let chpath = path.join(src,childrens[i]);
+            untreefyDir(chpath,dest,chobj);
+            node.children.push(chobj);
         }
     }
-    
 }
 
 
